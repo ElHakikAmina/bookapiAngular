899ae8a340e41d18e958ae6ce8aeb48b41d90deb
EL HAKIK AMINA <amina.el.hakik2@gmail.com>
2023-11-08 17:29:00 +0000

    show books

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -7,6 +7,21 @@ import {Book} from './book';
     templateUrl:'./book.component.html',
     styleUrls:['./book.component.css']
 })
-export class BookComponent{
+export class BookComponent implements OnInit{
 
+    books: Book[];
+    constructor(private _bookService: BookService){}
+
+    ngOnInit(): void{
+        this.getBooks();
+    }
+
+
+
+    getBooks(): void{
+        this._bookService.getAllBooks()
+        .subscribe((bookData) => {this.books =boookData, console.log(bookData)}, (error) =>{
+            console.log(error);
+        });
+    }
 }
